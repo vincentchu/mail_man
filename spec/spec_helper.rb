d5a73c699692912e7c93ce0138bde086010f7935
vincentchu <vince@posterous.com>
2011-07-21 15:11:51 -0700

    Adding tags finding and pagination

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,16 @@ require 'rubygems'
 require 'mail_man'
 
 $redis = Redis.new(:host => "localhost", :port => 6379)
+
+def store_many_mesgs_for!(tag, n = 100)
+  n.times.each do |i|
+    subj = "message_#{i}"
+    mesg_id = "<message.id.is.#{i}@foo.com>"
+
+    MailMan::Message.new(
+      :subject    => subj,
+      :message_id => mesg_id,
+      :tags       => [tag]
+    ).save!
+  end
+end
